a22bfb1384ac9dbe5fef0107074daeb07f03c0e2
WriteCoin <sovtsardom@gmail.com>
2020-10-25 13:15:27 +0300

    Doodad исправлен

--- API/doodad.lua
+++ API/doodad.lua
@@ -2,27 +2,27 @@
 -- Doodad API
 --
 
-Doodad = (function()
-    local t = {}
+Doodad = {}
 
-    ---@param x real
-    ---@param y real
-    ---@param radius real
-    ---@param doodadID integer
-    ---@param nearestOnly boolean
-    ---@param animName string
-    ---@param animRandom boolean
-    function t.setAnimation(x, y, radius, doodadID, nearestOnly, animName, animRandom)
-        SetDoodadAnimation(x, y, radius, doodadID, nearestOnly, animName, animRandom)
-    end
+local t = Doodad
 
-    ---@param r rect
-    ---@param doodadID integer
-    ---@param animName string
-    ---@param animRandom boolean
-    function t.setAnimationRect(r, doodadID, animName, animRandom)
-        SetDoodadAnimationRect(r, doodadID, animName, animRandom)
-    end
+---@param x real
+---@param y real
+---@param radius real
+---@param doodadID integer
+---@param nearestOnly boolean
+---@param animName string
+---@param animRandom boolean
+function t.setAnimation(x, y, radius, doodadID, nearestOnly, animName, animRandom)
+    SetDoodadAnimation(x, y, radius, doodadID, nearestOnly, animName, animRandom)
+end
 
-    return t
-end)()
+---@param r rect
+---@param doodadID integer
+---@param animName string
+---@param animRandom boolean
+function t.setAnimationRect(r, doodadID, animName, animRandom)
+    SetDoodadAnimationRect(r, doodadID, animName, animRandom)
+end
+
+return t
